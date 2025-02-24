f5c972c833eb763e82d38b9e95aa95c36dcd8c92
harikrir <hkrishnan254@gmail.com>
2025-02-24 15:09:51 +0300

    Delete src/ios/WalletExtensionPlugin.h

--- src/ios/WalletExtensionPlugin.h
+++
@@ -1,9 +0,0 @@
-#import <Cordova/CDV.h>
-#import <PassKit/PassKit.h>
-
-@interface WalletExtensionPlugin : CDVPlugin
-
-- (void)updatePassInWallet:(CDVInvokedUrlCommand*)command;
-- (void)handleDeepLink:(CDVInvokedUrlCommand*)command;
-
-@end
