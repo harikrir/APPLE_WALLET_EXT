29e73771066d99abca8ca2cff86d9e122d7ed7b3
harikrir <hkrishnan254@gmail.com>
2025-02-24 10:35:30 +0300

    Update MyAppleWalletPlugin.h

--- src/ios/MyAppleWalletPlugin.h
+++ src/ios/MyAppleWalletPlugin.h
@@ -1,5 +1,8 @@
 #import <Cordova/CDV.h>
 #import <PassKit/PassKit.h>
+#import "Foundation/Foundation.h"
+#import <Cordova/CDVPlugin.h>
+#import <WatchConnectivity/WatchConnectivity.h>
 
 @interface MyAppleWalletPlugin : CDVPlugin <PKAddPaymentPassViewControllerDelegate>
 @property (nonatomic, strong) CDVInvokedUrlCommand *pendingCommand;
