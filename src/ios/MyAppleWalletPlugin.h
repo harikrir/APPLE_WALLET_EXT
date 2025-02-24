7fe7bd46cba9ec596714ef534e9eef446b303ed5
harikrir <hkrishnan254@gmail.com>
2025-02-24 14:08:30 +0300

    Delete src/ios/MyAppleWalletPlugin.h

--- src/ios/MyAppleWalletPlugin.h
+++
@@ -1,9 +0,0 @@
-#import <Cordova/CDV.h>
-#import <PassKit/PassKit.h>
-#import "Foundation/Foundation.h"
-#import <Cordova/CDVPlugin.h>
-#import <WatchConnectivity/WatchConnectivity.h>
-
-@interface MyAppleWalletPlugin : CDVPlugin <PKAddPaymentPassViewControllerDelegate>
-@property (nonatomic, strong) CDVInvokedUrlCommand *pendingCommand;
-@end
