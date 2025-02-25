9b0a6011726ca9c775cbbc07a3bba8f04356c26f
harikrir <hkrishnan254@gmail.com>
2025-02-25 08:25:47 +0300

    Update WalletExtensionCardsPlugin.m

--- src/ios/WalletExtensionCardsPlugin.m
+++ src/ios/WalletExtensionCardsPlugin.m
@@ -1,12 +1,44 @@
 #import "WalletExtensionCardsPlugin.h"
 #import <LocalAuthentication/LocalAuthentication.h>
 #import <PassKit/PassKit.h>
+#import <WatchConnectivity/WatchConnectivity.h>
 
 @interface WalletExtensionCardsPlugin () <PKAddPaymentPassViewControllerDelegate>
 @property (nonatomic, strong) CDVInvokedUrlCommand *pendingCommand;
 @end
 
 @implementation WalletExtensionCardsPlugin
+
+
+
+
+- (NSString *) getCardFPAN:(NSString *) cardSuffix{
+    
+    PKPassLibrary *passLibrary = [[PKPassLibrary alloc] init];
+    NSArray<PKPass *> *paymentPasses = [passLibrary passesOfType:PKPassTypePayment];
+    for (PKPass *pass in paymentPasses) {
+        PKPaymentPass * paymentPass = [pass paymentPass];
+        if([[paymentPass primaryAccountNumberSuffix] isEqualToString:cardSuffix])
+            return [paymentPass primaryAccountIdentifier];
+    }
+    
+    if (WCSession.isSupported) { // check if the device support to handle an Apple Watch
+        WCSession *session = [WCSession defaultSession];
+        [session setDelegate:self.appDelegate];
+        [session activateSession];
+        
+        if ([session isPaired]) { // Check if the iPhone is paired with the Apple Watch
+            paymentPasses = [passLibrary remotePaymentPasses];
+            for (PKPass *pass in paymentPasses) {
+                PKPaymentPass * paymentPass = [pass paymentPass];
+                if([[paymentPass primaryAccountNumberSuffix] isEqualToString:cardSuffix])
+                    return [paymentPass primaryAccountIdentifier];
+            }
+        }
+    }
+    
+    return nil;
+}
 
 - (void)addCardToWallet:(CDVInvokedUrlCommand*)command {
     NSDictionary* cardDetails = [command.arguments objectAtIndex:0];
@@ -23,7 +55,7 @@
     config.primaryAccountSuffix =  [cardDetails objectForKey:@"primaryAccountSuffix"];
      config.localizedDescription = [cardDetails objectForKey:@"localizedDescription"];
     config.paymentNetwork = PKPaymentNetworkMasterCard;
-// config.primaryAccountIdentifier = [self getCardFPAN:config.primaryAccountSuffix];
+ config.primaryAccountIdentifier = [self getCardFPAN:config.primaryAccountSuffix];
     
  //   config.requiresAuthentication = YES; // Enable external authentication
     
