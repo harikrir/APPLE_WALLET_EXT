7d970d7d3312ab77dbf090d890296eeb6c5670a3
harikrir <hkrishnan254@gmail.com>
2025-12-24 11:31:49 +0300

    Update KFHWalletPlugin.swift

--- src/ios/KFHWalletPlugin.swift
+++ src/ios/KFHWalletPlugin.swift
@@ -1,58 +1,46 @@
 import Foundation
 import PassKit
 @objc(KFHWalletPlugin) class KFHWalletPlugin : CDVPlugin {
-   @objc(canAddCard:)
-   func canAddCard(command: CDVInvokedUrlCommand) {
-       let isAvailable = PKAddPaymentPassViewController.canAddPaymentPass()
-       let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: isAvailable)
-       self.commandDelegate.send(result, callbackId: command.callbackId)
-   }
-   @objc(startProvisioning:)
-   func startProvisioning(command: CDVInvokedUrlCommand) {
-       guard let cardId = command.arguments[0] as? String,
-             let cardName = command.arguments[1] as? String else {
-           let result = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Invalid Arguments")
-           self.commandDelegate.send(result, callbackId: command.callbackId)
-           return
-       }
-       let config = PKAddPaymentPassRequestConfiguration(encryptionScheme: .ECC_V2)
-       config?.cardholderName = "KFH Customer"
-       config?.primaryAccountSuffix = String(cardId.suffix(4))
-       config?.localizedDescription = cardName
-       guard let vc = PKAddPaymentPassViewController(requestConfiguration: config!, delegate: self) else {
-           let result = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Could not load Apple UI")
-           self.commandDelegate.send(result, callbackId: command.callbackId)
-           return
-       }
-       self.viewController.present(vc, animated: true, completion: nil)
-       // Send a success to JS that the UI opened
-       let result = CDVPluginResult(status: CDVCommandStatus_OK)
-       self.commandDelegate.send(result, callbackId: command.callbackId)
-   }
+  @objc(canAddCard:)
+  func canAddCard(command: CDVInvokedUrlCommand) {
+      let isAvailable = PKAddPaymentPassViewController.canAddPaymentPass()
+      let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: isAvailable)
+      self.commandDelegate.send(result, callbackId: command.callbackId)
+  }
+  @objc(startProvisioning:)
+  func startProvisioning(command: CDVInvokedUrlCommand) {
+      guard let cardId = command.arguments[0] as? String,
+            let cardName = command.arguments[1] as? String else {
+          let result = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Invalid Arguments")
+          self.commandDelegate.send(result, callbackId: command.callbackId)
+          return
+      }
+      let config = PKAddPaymentPassRequestConfiguration(encryptionScheme: .ECC_V2)
+      config?.cardholderName = "KFH Customer"
+      config?.primaryAccountSuffix = String(cardId.suffix(4))
+      config?.localizedDescription = cardName
+      guard let configData = config,
+            let vc = PKAddPaymentPassViewController(requestConfiguration: configData, delegate: self) else {
+          let result = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Could not load Apple UI")
+          self.commandDelegate.send(result, callbackId: command.callbackId)
+          return
+      }
+      self.viewController.present(vc, animated: true, completion: nil)
+      let result = CDVPluginResult(status: CDVCommandStatus_OK)
+      self.commandDelegate.send(result, callbackId: command.callbackId)
+  }
 }
 extension KFHWalletPlugin: PKAddPaymentPassViewControllerDelegate {
+   // ONLY ONE INSTANCE OF THIS FUNCTION IS ALLOWED
    func addPaymentPassViewController(_ controller: PKAddPaymentPassViewController, generateRequestWithCertificateChain certificates: [Data], nonce: Data, nonceSignature: Data, completionHandler: @escaping (PKAddPaymentPassRequest) -> Void) {
-       // IMPORTANT: In production, you send these 'certificates', 'nonce', and 'nonceSignature'
-       // to your AUB/KFH backend. Your backend returns the encrypted data.
-       // For now, we create an empty request to prevent the UI from hanging.
        let request = PKAddPaymentPassRequest()
-       // Once your backend API is ready, you will populate these:
-       // request.encryptedPassData = ...
-       // request.activationData = ...
-       // request.ephemeralPublicKey = ...
-       completionHandler(request)
-   }
-   
-
-func addPaymentPassViewController(_ controller: PKAddPaymentPassViewController, generateRequestWithCertificateChain certificates: [Data], nonce: Data, nonceSignature: Data, completionHandler: @escaping (PKAddPaymentPassRequest) -> Void) {
-       // We MUST return an object to prevent the build error and UI hang
-       let request = PKAddPaymentPassRequest()
-       // In a real scenario, you populate these from your KFH API
-       // For now, we provide empty Data objects to satisfy the compiler
+       // Populate with empty Data to satisfy the compiler for now
        request.encryptedPassData = Data()
        request.activationData = Data()
        request.ephemeralPublicKey = Data()
        completionHandler(request)
    }
-   
+   func addPaymentPassViewController(_ controller: PKAddPaymentPassViewController, didFinishWith pass: PKPaymentPass?, error: Error?) {
+       controller.dismiss(animated: true, completion: nil)
+   }
 }
