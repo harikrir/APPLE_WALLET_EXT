7c354f00f70b3d1c7034858aafb3c3ac8b92c7a4
harikrir <hkrishnan254@gmail.com>
2025-12-28 14:55:13 +0300

    Update KFHWalletPlugin.swift

--- src/ios/KFHWalletPlugin.swift
+++ src/ios/KFHWalletPlugin.swift
@@ -1,32 +1,52 @@
 import Foundation
 import PassKit
 @objc(KFHWalletPlugin) class KFHWalletPlugin : CDVPlugin {
-   var callbackId: String?
-   let suite = UserDefaults(suiteName: "group.com.aub.mobilebanking.uat.bh")
+   var currentCallbackId: String?
+   let sharedSuite = UserDefaults(suiteName: "group.com.aub.mobilebanking.uat.bh")
+   @objc(canAddCard:)
+   func canAddCard(command: CDVInvokedUrlCommand) {
+       let isAvailable = PKAddPaymentPassViewController.canAddPaymentPass()
+       self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_OK, messageAs: isAvailable), callbackId: command.callbackId)
+   }
    @objc(setAuthToken:)
    func setAuthToken(command: CDVInvokedUrlCommand) {
        let token = command.arguments[0] as? String
-       suite?.set(token, forKey: "AUB_Auth_Token")
-       suite?.synchronize()
+       sharedSuite?.set(token, forKey: "AUB_Auth_Token")
+       sharedSuite?.synchronize()
        self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_OK), callbackId: command.callbackId)
    }
    @objc(startProvisioning:)
    func startProvisioning(command: CDVInvokedUrlCommand) {
-       self.callbackId = command.callbackId
-       let cardId = command.arguments[0] as? String ?? ""
-       let cardName = command.arguments[1] as? String ?? "KFH Card"
-       let config = PKAddPaymentPassRequestConfiguration(encryptionScheme: .ECC_V2)!
-       config.primaryAccountSuffix = String(cardId.suffix(4))
-       config.localizedDescription = cardName
-       suite?.set(cardId, forKey: "ACTIVE_CARD")
-       let vc = PKAddPaymentPassViewController(requestConfiguration: config, delegate: self)!
-       self.viewController.present(vc, animated: true)
+       self.currentCallbackId = command.callbackId
+       guard let cardId = command.arguments[0] as? String,
+             let cardName = command.arguments[1] as? String else {
+           self.sendError("Invalid Arguments")
+           return
+       }
+       let config = PKAddPaymentPassRequestConfiguration(encryptionScheme: .ECC_V2)
+       config?.cardholderName = "KFH Customer"
+       config?.primaryAccountSuffix = String(cardId.suffix(4))
+       config?.localizedDescription = cardName
+       sharedSuite?.set(cardId, forKey: "ACTIVE_CARD_ID")
+       if let vc = PKAddPaymentPassViewController(requestConfiguration: config!, delegate: self) {
+           self.viewController.present(vc, animated: true, completion: nil)
+       } else {
+           self.sendError("Apple Pay UI Unavailable")
+       }
+   }
+   func sendError(_ message: String) {
+       let result = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: message)
+       self.commandDelegate.send(result, callbackId: self.currentCallbackId)
    }
 }
+// MARK: - Delegate Extension
 extension KFHWalletPlugin: PKAddPaymentPassViewControllerDelegate {
    func addPaymentPassViewController(_ controller: PKAddPaymentPassViewController, generateRequestWithCertificateChain certificates: [Data], nonce: Data, nonceSignature: Data, completionHandler: @escaping (PKAddPaymentPassRequest) -> Void) {
-       let cardId = suite?.string(forKey: "ACTIVE_CARD") ?? ""
-       let token = suite?.string(forKey: "AUB_Auth_Token") ?? ""
+       guard let token = sharedSuite?.string(forKey: "AUB_Auth_Token"),
+             let cardId = sharedSuite?.string(forKey: "ACTIVE_CARD_ID") else {
+           completionHandler(PKAddPaymentPassRequest())
+           return
+       }
        let body: [String: Any] = [
            "cardId": cardId,
            "certificates": certificates.map { $0.base64EncodedString() },
@@ -36,23 +56,28 @@ extension KFHWalletPlugin: PKAddPaymentPassViewControllerDelegate {
        var request = URLRequest(url: URL(string: "https://api.aub.com.bh/v1/wallet/encrypt")!)
        request.httpMethod = "POST"
        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+       request.setValue("application/json", forHTTPHeaderField: "Content-Type")
        request.httpBody = try? JSONSerialization.data(withJSONObject: body)
        URLSession.shared.dataTask(with: request) { data, _, _ in
-           let res = PKAddPaymentPassRequest()
-           if let data = data, let dec = try? JSONDecoder().decode(KFHEncryptionResponse.self, from: data) {
-               res.encryptedPassData = Data(base64Encoded: dec.encryptedPassData)
-               res.activationData = Data(base64Encoded: dec.activationData)
-               res.ephemeralPublicKey = Data(base64Encoded: dec.ephemeralPublicKey)
+           let addRequest = PKAddPaymentPassRequest()
+           if let data = data, let res = try? JSONDecoder().decode(KFHEncryptionResponse.self, from: data) {
+               addRequest.encryptedPassData = Data(base64Encoded: res.encryptedPassData)
+               addRequest.activationData = Data(base64Encoded: res.activationData)
+               addRequest.ephemeralPublicKey = Data(base64Encoded: res.ephemeralPublicKey)
            }
-           completionHandler(res)
+           completionHandler(addRequest)
        }.resume()
    }
    func addPaymentPassViewController(_ controller: PKAddPaymentPassViewController, didFinishWith pass: PKPaymentPass?, error: Error?) {
        controller.dismiss(animated: true) {
-           self.commandDelegate.send(CDVPluginResult(status: (pass != nil) ? .ok : .error), callbackId: self.callbackId)
+           let result = (pass != nil) ? CDVPluginResult(status: CDVCommandStatus_OK) : CDVPluginResult(status: CDVCommandStatus_ERROR)
+           self.commandDelegate.send(result, callbackId: self.currentCallbackId)
        }
    }
 }
+// Global scope structs
 struct KFHEncryptionResponse: Codable {
-   let encryptedPassData, activationData, ephemeralPublicKey: String
+   let encryptedPassData: String
+   let activationData: String
+   let ephemeralPublicKey: String
 }
