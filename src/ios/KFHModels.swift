2a8bd90bfa027a4d6db27d76639e7dd5b6b7c7c6
harikrir <hkrishnan254@gmail.com>
2025-12-29 12:42:34 +0300

    Update KFHModels.swift

--- src/ios/KFHModels.swift
+++ src/ios/KFHModels.swift
@@ -1,11 +1,24 @@
 import Foundation
-struct KFHEncryptionResponse: Codable {
-   let encryptedPassData: String
-   let activationData: String
-   let ephemeralPublicKey: String
+// MARK: - Card List Model
+public struct KFHCardEntry: Codable {
+   public let id: String
+   public let name: String
+   public let last4: String
+   // Required: Public structs need a public init to be created in other modules
+   public init(id: String, name: String, last4: String) {
+self.id = id
+       self.name = name
+       self.last4 = last4
+   }
 }
-struct KFHCardEntry: Codable {
-   let id: String
-   let name: String
-   let last4: String
+// MARK: - Encryption Response Model
+public struct KFHEncryptionResponse: Codable {
+   public let encryptedPassData: String
+   public let activationData: String
+   public let ephemeralPublicKey: String
+   public init(encryptedPassData: String, activationData: String, ephemeralPublicKey: String) {
+       self.encryptedPassData = encryptedPassData
+       self.activationData = activationData
+       self.ephemeralPublicKey = ephemeralPublicKey
+   }
 }
