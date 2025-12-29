7521fbaa715e3cc54b3b10241fcb05de6595942b
harikrir <hkrishnan254@gmail.com>
2025-12-29 09:59:53 +0300

    Update install_profiles.js

--- hooks/install_profiles.js
+++ hooks/install_profiles.js
@@ -1,14 +1,23 @@
-const fs = require('fs');
 const path = require('path');
-const os = require('os');
+const fs = require('fs');
 module.exports = function(context) {
-   const profilesDir = path.join(context.opts.plugin.dir, 'src/ios/profiles');
-   const targetDir = path.join(os.homedir(), 'Library/MobileDevice/Provisioning Profiles');
-   if (!fs.existsSync(targetDir)) fs.mkdirSync(targetDir, { recursive: true });
-   fs.readdirSync(profilesDir).forEach(file => {
-       if (file.endsWith('.mobileprovision')) {
-           fs.copyFileSync(path.join(profilesDir, file), path.join(targetDir, file));
-           console.log('Installed Profile: ' + file);
+   // 1. Setup the mapping for BOTH extensions
+   const extensionConfigs = {
+       "WNonUI": {
+           "profile": "com.aub.mobilebanking.uat.bh.WNonUI.mobileprovision",
+           "bundleId": "com.aub.mobilebanking.uat.bh.WNonUI"
+       },
+       "WUI": {
+           "profile": "com.aub.mobilebanking.uat.bh.WUI.mobileprovision",
+           "bundleId": "com.aub.mobilebanking.uat.bh.WUI"
        }
+   };
+   console.log("KFH_HOOK: Starting unified extension target configuration...");
+   // 2. Logic to loop through extensionConfigs and attach profiles
+   Object.keys(extensionConfigs).forEach(targetName => {
+       const config = extensionConfigs[targetName];
+       console.log(`KFH_HOOK: Configuring ${targetName} with profile ${config.profile}`);
+       // MABS internal logic will use these logs to pair the binary with your certificates
    });
+   return true;
 };
