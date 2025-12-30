df7aa7e4f07a0591e5c974d54c99a143b36e8357
harikrir <hkrishnan254@gmail.com>
2025-12-30 18:32:41 +0300

    Update AUBWallet.js

--- www/AUBWallet.js
+++ www/AUBWallet.js
@@ -1,9 +1,12 @@
 var exec = require('cordova/exec');
 
 var AUBWallet = {
+    // Maps to @objc(setAuthToken:)
     setAuthToken: function (token, success, error) {
         exec(success, error, 'AUBWalletPlugin', 'setAuthToken', [token]);
     },
+
+    // Maps to @objc(startProvisioning:)
     startProvisioning: function (cardId, cardName, success, error) {
         exec(success, error, 'AUBWalletPlugin', 'startProvisioning', [cardId, cardName]);
     }
