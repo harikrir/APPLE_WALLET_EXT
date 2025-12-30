3c7f867a468adb0be3d68d339065aa1c6bf6e9e9
harikrir <hkrishnan254@gmail.com>
2025-12-30 08:55:54 +0300

    Update AppGroupManager.swift

--- src/ios/AppGroupManager.swift
+++ src/ios/AppGroupManager.swift
@@ -1,3 +1,20 @@
 import Foundation
-let appGroupID: String = "group.com.aub.mobilebanking.uat.bh"
-let appGroupSharedDefaults: UserDefaults = UserDefaults(suiteName: appGroupID)!
+
+class AppGroupManager {
+    static let shared = AppGroupManager()
+    private let groupID = "group.com.aub.mobilebanking.uat.bh"
+    private let defaults: UserDefaults?
+
+    private init() {
+        self.defaults = UserDefaults(suiteName: groupID)
+    }
+
+    func saveCard(id: String) {
+        defaults?.set(id, forKey: "ACTIVE_CARD_ID")
+        defaults?.synchronize() // Ensures data is written to disk immediately
+    }
+
+    func getCard() -> String? {
+        return defaults?.string(forKey: "ACTIVE_CARD_ID")
+    }
+}
