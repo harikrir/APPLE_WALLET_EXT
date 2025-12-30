658271221820318e5c10a58c940c51fdb4467cf2
harikrir <hkrishnan254@gmail.com>
2025-12-30 13:07:08 +0300

    Update AUBLog.swift

--- src/ios/WNonUIExt/Models/AUBLog.swift
+++ src/ios/WNonUIExt/Models/AUBLog.swift
@@ -1,9 +1,12 @@
 import Foundation
 import os
-
 public struct AUBLog {
-    private static let subsystem = "com.aub.mobilebanking.uat.bh"
-    static let plugin = Logger(subsystem: subsystem, category: "App-Plugin")
-    static let nonUI = Logger(subsystem: subsystem, category: "Ext-NonUI")
-    static let ui = Logger(subsystem: subsystem, category: "Ext-UI")
+   private static let subsystem = "com.aub.mobilebanking.uat.bh"
+   static let plugin = Logger(subsystem: subsystem, category: "App-Plugin")
+   static let nonUI = Logger(subsystem: subsystem, category: "Ext-NonUI")
+   static let ui = Logger(subsystem: subsystem, category: "Ext-UI")
+   // Use privacy: .public so the message isn't hidden in the Console
+   public static func log(_ message: String) {
+       nonUI.notice("💳 [AUB]: \(message, privacy: .public)")
+   }
 }
